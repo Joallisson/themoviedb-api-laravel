82744efee316e79b0c1040469fc80d22cb1eb8a5
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-07-28 21:10:44 -0300

    create: inial commit

---
+++ app/Http/Controllers/v1/users/UserController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\v1\users;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    protected $user;
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+
+    public function index(){
+
+        $user = $this->user->get();
+
+        return response()->json($user);
+    }
+
+    public function show($id){
+
+        try {
+            $user = $this->user->findOrFail($id);
+
+            return response()->json([
+                'user' => $user
+            ]);
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function store(Request $request){
+
+        $data = $request->all();
+
+        try {
+
+            $data["password"] = bcrypt($data["password"]);
+
+            $user = $this->user->create($data);
+
+            return response()->json([
+                'message' => 'usuário cadastrado com sucesso',
+                'data' => $user
+            ]);
+
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function update(Request $request, $id){
+
+        try {
+            $data = $request->all();
+            $user = $this->user->findOrFail($id);
+
+            $user->update($data);
+
+            return response()->json([
+                'message' => 'user atualizado com sucesso',
+                'data' => $user
+            ]);
+
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+
+    }
+
+    public function destroy($id){
+
+        try {
+            $user = $this->user->findOrFail($id);
+            $user->delete();
+
+            return response()->json([
+                'message' => 'Usuário deletado com sucesso'
+            ]);
+
+        } catch (\Throwable $th) {
+
+            return $th->getMessage();
+        }
+    }
+}
---
+++ database/migrations/2022_07_20_001024_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->integer('score');
+            $table->boolean('whatched');
+            $table->unsignedBigInteger('media_id');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
